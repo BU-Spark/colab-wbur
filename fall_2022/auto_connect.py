026fa2fc41cb35a491c49359863b37b5f891781e
zikunwang-terry <terryiochina@gmail.com>
2022-11-04 14:22:06 -0400

    update

--- fall_2022/auto_connect.py
+++ fall_2022/auto_connect.py
@@ -7,6 +7,9 @@ import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
 
+from pathlib import Path
+from tqdm import tqdm
+
 with open("./config/sql_login.txt", newline="") as f:
   sql_login = f.readline()
   
@@ -14,27 +17,36 @@ print(sql_login)
 
 engine = create_engine(sql_login)
 
-#sql_command = '''
-#SELECT ccmi.post_id, cpi.party_name , case_number, case_type, case_status, file_date, status_date
-#FROM wp_courtdocs.cdocs_case_meta_index ccmi
-#INNER JOIN wp_courtdocs.cdocs_party_assignment_index cpai ON ccmi.post_id = cpai.case_id 
-#INNER JOIN wp_courtdocs.cdocs_party_index cpi ON cpi.post_id = cpai.party_id
-#WHERE cpai.party_type LIKE '%Plaintiff%'
-#	AND cpi.party_name LIKE '%wells%fargo%'
-#'''
+def query_company_name(name):
+  print(f"{name}: start querying")
 
-sql_command = '''
-SELECT ccmi.post_id, cpi.party_name , case_number, case_type, case_status, file_date, status_date
-FROM wp_courtdocs.cdocs_case_meta_index ccmi
-INNER JOIN wp_courtdocs.cdocs_party_assignment_index cpai ON ccmi.post_id = cpai.case_id 
-INNER JOIN wp_courtdocs.cdocs_party_index cpi ON cpi.post_id = cpai.party_id
-LIMIT 1
-'''
+  query_name = "%" + name.replace(" ", "%") + "%"
+  print(f"{name}: query name ({query_name})")
 
-df = pd.read_sql(sql_command, engine) #read the entire table
-#
-#
-#
-#print(df)
+  sql_command = f'''
+  SELECT ccmi.post_id, cpi.party_name , case_number, case_type, case_status, file_date, status_date
+  FROM wp_courtdocs.cdocs_case_meta_index ccmi
+  INNER JOIN wp_courtdocs.cdocs_party_assignment_index cpai ON ccmi.post_id = cpai.case_id 
+  INNER JOIN wp_courtdocs.cdocs_party_index cpi ON cpi.post_id = cpai.party_id
+  WHERE cpai.party_type LIKE '%Plaintiff%'
+  	AND cpi.party_name LIKE '{query_name}'
+  '''
+
+  df = pd.DataFrame()
+  for chunk in pd.read_sql(sql_command, engine, chunksize=50):
+    df=pd.concat([df, chunk])
+
+  print(f"{name}: finished querying, saving result.")
+  file_name = name.replace(" ", "_")
+
+  filepath = Path(f"./data/debt_collector_query_result/{file_name}.csv")
+  filepath.parent.mkdir(parents=True, exist_ok=True)  
+  df.to_csv(filepath)
+
+  print(f"{name}: save complete.")
+
+#with open("./data/debt_collector_query_result/wells_fargo.csv", "w") as f:
+
+query_company_name("Wells Fargo")
 
 #conn = engine.connect()
